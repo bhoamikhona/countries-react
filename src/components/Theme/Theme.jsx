fa7ce7270f22e27c9ed57382c8505945e47d8881
Bhoami K Khona <bhoamikhona@gmail.com>
2024-04-15 12:38:31 +0530

    header complete

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,12 +1,40 @@
-import React from "react";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import React, { useEffect, useState } from "react";
+import "./Theme.css";
+import { ReactComponent as MoonOutline } from "../../assets/images/moon-outline.svg";
+import { ReactComponent as MoonFilled } from "../../assets/images/moon-filled.svg";
 
 function Theme() {
+  const [darkMode, setDarkMode] = useState(function () {
+    const storedTheme = localStorage.getItem("theme");
+    const prefersDarkMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return storedTheme === "dark" || (storedTheme === null && prefersDarkMode);
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem("theme", darkMode ? "dark" : "light");
+      document.body.setAttribute("data-theme", darkMode ? "dark" : "light");
+    },
+    [darkMode]
+  );
+
+  const toggleTheme = function () {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="theme">
-      <DarkModeOutlinedIcon fontSize="large" className="moon" />
+    <button className="theme" onClick={toggleTheme}>
+      <span className="theme__icon-container">
+        {darkMode ? (
+          <MoonFilled className="theme__icon" />
+        ) : (
+          <MoonOutline className="theme__icon" />
+        )}
+      </span>
       <span className="theme__text">Dark Mode</span>
-    </div>
+    </button>
   );
 }
 
