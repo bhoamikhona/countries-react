a41bcecf5ca197e797a58407685fbbcd714a7045
Bhoami K Khona <bhoamikhona@gmail.com>
2024-04-17 07:24:08 +0530

    fixed a css issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Country from "./pages/Country/Country.jsx";
 import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+// import Footer from "./components/Footer/Footer.jsx";
 
 export const formatNumber = (number) => Intl.NumberFormat().format(number);
 
