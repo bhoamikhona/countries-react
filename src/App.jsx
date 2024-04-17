8958fe1f0a10cf8cc93ec2fff4eacb5cd6a61a69
Bhoami K Khona <bhoamikhona@gmail.com>
2024-04-17 07:03:41 +0530

    commented footer - need to fix its position

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/country/:name" element={<Country />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
